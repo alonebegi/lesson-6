27de0362b30c02390301c536ed941b092c4627dc
Sunnatillo Azimjonov <monomusix001@gmail.com>
2024-04-04 16:39:46 +0300

    function fn 22

---
+++ app.js
@@ -0,0 +1,72 @@
+
+// masala7
+// const student = {
+// 	name : "David Rayy",
+// 	sclass : "VI",
+// 	rollno : 12 
+// }
+// console.log(student);
+
+// 8masala
+// const student = {
+// 	name : "David Rayy",
+// 	class : "VI",
+// 	rollno : 12 
+// };
+// delete student.rollno
+// console.log(student);
+
+// 9masala
+// const student = {
+// 	name : "David Rayy",
+// 	sclass : "VI",
+// 	rollno : 12 ,
+// };
+// let length = Object.keys(student).length;
+// console.log(length);
+
+// masala10
+// const car = {
+// 	rusumi: 'nexia 3',
+// 	rangi: 'Mokry',
+// 	// methods
+//   getOvoz : function() {
+//     start : "vrrr vrrr";
+//   },
+//   getDrive : function() {
+//     drive : "driving";
+//   },
+//   getStop : function() {
+//     stop : "stoping";
+//   }
+// }
+// car.getOvoz();
+// car.getDrive();
+// car.getStop();
+// console.log(car.getOvoz);   //ovoz orniga stop yoki drive ni qoyselar ozgaradi
+
+// masala 12 tushunmadm
+
+
+// let person = {
+//   city: "Andijan",
+//   age: 17,
+//   name: "Bek",
+//   adress : {},
+//   fn : function() {
+//    console.log(fn.surname = "Azimjonov");
+//   }
+// };
+
+// function fn()
+
+
+
+
+// // for(let key in person.adress){
+// //     console.log(person.adress[key]);
+// // }
+
+
+
+
